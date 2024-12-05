adfea49d8ed33378bbd54b8bdba1aa9c71b9a057
WwzFwz <dzakyaureliafawwaz@gmail.com>
2024-12-05 10:42:08 +0700

    fix navbar layout

--- src/frontend/components/sidebar.py
+++ src/frontend/components/sidebar.py
@@ -1,3 +1,142 @@
+# from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QLabel, 
+#                            QHBoxLayout, QFrame, QSizePolicy)
+# from PyQt5.QtCore import Qt, pyqtSignal
+# from PyQt5.QtGui import QFont, QIcon
+# import os
+
+# class Sidebar(QWidget):
+#     pageChanged = pyqtSignal(int)
+    
+#     def __init__(self):
+#         super().__init__()
+#         self.setFixedWidth(350)  # Perlebar jadi 350px
+#         current_file = os.path.abspath(__file__)
+#         frontend_dir = os.path.dirname(os.path.dirname(current_file))
+#         src_dir = os.path.dirname(frontend_dir)
+#         root_dir = os.path.dirname(src_dir)
+#         self.img_path = os.path.join(root_dir, 'img')
+#         self.setup_ui()
+        
+#     def setup_ui(self):
+#         self.inter_font = QFont("Arial", 12)
+        
+#         layout = QVBoxLayout(self)
+#         layout.setSpacing(15)
+#         layout.setContentsMargins(20, 20, 20, 20)
+        
+#         # Dashboard label
+#         dashboard_container = QWidget()
+#         dashboard_container.setStyleSheet("""
+#             background-color: #59694D; 
+#             border-radius: 12px;
+#             margin-right: 0px;
+#             padding-right: 0px;
+#         """)
+#         dashboard_layout = QHBoxLayout(dashboard_container)
+#         dashboard_layout.setContentsMargins(15, 10, 15, 10)
+        
+#         dashboard_label = QLabel("Dashboard")
+#         dashboard_label.setFont(self.inter_font)
+#         dashboard_label.setStyleSheet("color: white; font-weight: bold;")
+#         dashboard_layout.addWidget(dashboard_label)
+#         layout.addWidget(dashboard_container)
+        
+#         # Logo and Title
+#         logo_layout = QHBoxLayout()
+#         logo = QLabel()
+#         logo_path = os.path.join(self.img_path, 'logo.png')
+#         if os.path.exists(logo_path):
+#             logo_pixmap = QIcon(logo_path).pixmap(30, 30)
+#             logo.setPixmap(logo_pixmap)
+        
+#         title = QLabel()
+#         title_font = QFont("Arial", 14)
+#         title.setFont(title_font)
+#         title.setText('<span style="color: #59694D;">Groo</span><span style="color: #6C4530;">topia</span>')
+#         title.setTextFormat(Qt.RichText)
+        
+#         logo_layout.addWidget(logo)
+#         logo_layout.addWidget(title)
+#         logo_layout.addStretch()
+#         layout.addLayout(logo_layout)
+        
+#         layout.addSpacing(25)
+        
+#         # Navigation buttons
+#         nav_items = [
+#             ("Tanaman", os.path.join(self.img_path, 'tanaman.svg')),
+#             ("Catatan Perkembangan", os.path.join(self.img_path, 'book.svg')),
+#             ("Jadwal Perawatan", os.path.join(self.img_path, 'calender.svg'))
+#         ]
+        
+#         self.nav_buttons = []
+#         for idx, (text, icon_path) in enumerate(nav_items):
+#             button_container = QWidget()
+#             button_layout = QHBoxLayout(button_container)
+#             button_layout.setContentsMargins(0, 0, 0, 0)
+#             # button_container.setStyleSheet("""
+#             #     QWidget {
+#             #         background: transparent; 
+#             #         border: none;
+#             #     }
+#             # """)
+#             button_layout.setSpacing(0)
+#             btn = QPushButton(text)
+#             btn.setFont(self.inter_font)
+#             if os.path.exists(icon_path):
+#                 icon = QIcon(icon_path)
+#                 btn.setIcon(icon)
+#                 btn.setIconSize(QIcon(icon_path).pixmap(24, 24).size())
+#                 btn.setStyleSheet("""
+#                     QPushButton {
+#                         text-align: left;
+#                         padding: 15px;
+#                         padding-right: 30px;
+#                         border: none;
+#                         border-radius: 12px;
+#                         color: #333333;
+#                         min-height: 50px;
+#                         min-width: 310px;
+#                     }
+#                     QPushButton:hover {
+#                         background-color: #E8E8E8;
+#                     }
+#                     QPushButton:checked {
+#                         background-color: #59694D;
+#                         color: white;
+#                     }
+#                 """)
+                        
+#             btn.setCheckable(True)
+#             btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+#             btn.clicked.connect(lambda checked, i=idx: self.handle_button_click(i))
+            
+#             button_layout.addWidget(btn)
+#             self.nav_buttons.append(btn)
+#             layout.addWidget(button_container)
+            
+#         if self.nav_buttons:
+#             self.nav_buttons[0].setChecked(True)
+            
+#         layout.addStretch()
+        
+#         self.setStyleSheet("""
+#             QWidget {
+#                 background-color: #EBF1E6;
+
+#             }
+#             QWidget#sidebar {
+#                 min-height: 100vh;
+#             }
+#         """)
+#         self.setObjectName("sidebar")
+        
+#     def handle_button_click(self, index):
+#         for i, btn in enumerate(self.nav_buttons):
+#             btn.setChecked(i == index)
+#         self.pageChanged.emit(index)
+
+
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QLabel, 
                            QHBoxLayout, QFrame, QSizePolicy)
 from PyQt5.QtCore import Qt, pyqtSignal
@@ -9,7 +148,7 @@ class Sidebar(QWidget):
     
     def __init__(self):
         super().__init__()
-        self.setFixedWidth(350)  # Perlebar jadi 350px
+        self.setFixedWidth(350)
         current_file = os.path.abspath(__file__)
         frontend_dir = os.path.dirname(os.path.dirname(current_file))
         src_dir = os.path.dirname(frontend_dir)
@@ -18,28 +157,36 @@ class Sidebar(QWidget):
         self.setup_ui()
         
     def setup_ui(self):
-        self.inter_font = QFont("Arial", 12)
+        # Set font yang lebih tebal
+        self.inter_font = QFont("Arial", 12, QFont.Bold)
         
+        # Main layout tanpa margins
         layout = QVBoxLayout(self)
         layout.setSpacing(15)
-        layout.setContentsMargins(20, 20, 20, 20)
+        layout.setContentsMargins(0, 0, 0, 0)  # Hilangkan margins
         
         # Dashboard label
         dashboard_container = QWidget()
         dashboard_container.setStyleSheet("""
             background-color: #59694D; 
-            border-radius: 12px;
-            margin-right: 0px;
-            padding-right: 0px;
+            border-radius: 0px;
+            margin: 0px;
+            padding: 10px 20px;
         """)
         dashboard_layout = QHBoxLayout(dashboard_container)
         dashboard_layout.setContentsMargins(15, 10, 15, 10)
         
         dashboard_label = QLabel("Dashboard")
         dashboard_label.setFont(self.inter_font)
-        dashboard_label.setStyleSheet("color: white; font-weight: bold;")
+        dashboard_label.setStyleSheet("color: white;")
         dashboard_layout.addWidget(dashboard_label)
         layout.addWidget(dashboard_container)
+        
+        # Container untuk konten utama dengan padding
+        content_container = QWidget()
+        content_layout = QVBoxLayout(content_container)
+        content_layout.setContentsMargins(20, 20, 20, 20)
+        content_layout.setSpacing(15)
         
         # Logo and Title
         logo_layout = QHBoxLayout()
@@ -50,7 +197,7 @@ class Sidebar(QWidget):
             logo.setPixmap(logo_pixmap)
         
         title = QLabel()
-        title_font = QFont("Arial", 14)
+        title_font = QFont("Arial", 14, QFont.Bold)  # Buat lebih tebal
         title.setFont(title_font)
         title.setText('<span style="color: #59694D;">Groo</span><span style="color: #6C4530;">topia</span>')
         title.setTextFormat(Qt.RichText)
@@ -58,9 +205,9 @@ class Sidebar(QWidget):
         logo_layout.addWidget(logo)
         logo_layout.addWidget(title)
         logo_layout.addStretch()
-        layout.addLayout(logo_layout)
+        content_layout.addLayout(logo_layout)
         
-        layout.addSpacing(25)
+        content_layout.addSpacing(25)
         
         # Navigation buttons
         nav_items = [
@@ -71,79 +218,59 @@ class Sidebar(QWidget):
         
         self.nav_buttons = []
         for idx, (text, icon_path) in enumerate(nav_items):
-            button_container = QWidget()
-            button_layout = QHBoxLayout(button_container)
-            button_layout.setContentsMargins(0, 0, 0, 0)
-            # button_container.setStyleSheet("""
-            #     QWidget {
-            #         background: transparent; 
-            #         border: none;
-            #     }
-            # """)
-            button_layout.setSpacing(0)
             btn = QPushButton(text)
             btn.setFont(self.inter_font)
             if os.path.exists(icon_path):
                 icon = QIcon(icon_path)
                 btn.setIcon(icon)
                 btn.setIconSize(QIcon(icon_path).pixmap(24, 24).size())
-                btn.setStyleSheet("""
-                    QPushButton {
-                        text-align: left;
-                        padding: 15px;
-                        padding-right: 30px;
-                        border: none;
-                        border-radius: 12px;
-                        color: #333333;
-                        min-height: 50px;
-                        min-width: 310px;
-                    }
-                    QPushButton:hover {
-                        background-color: #E8E8E8;
-                    }
-                    QPushButton:checked {
-                        background-color: #59694D;
-                        color: white;
-                    }
-                """)
-                        
+            
+            btn.setStyleSheet("""
+                QPushButton {
+                    text-align: left;
+                    padding: 15px;
+                    padding-right: 30px;
+                    border: none;
+                    border-radius: 12px;
+                    color: #333333;
+                    min-height: 50px;
+                    min-width: 310px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: rgba(89, 105, 77, 0.1);
+                }
+                QPushButton:checked {
+                    background-color: #59694D;
+                    color: white;
+                }
+            """)
             btn.setCheckable(True)
             btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
             btn.clicked.connect(lambda checked, i=idx: self.handle_button_click(i))
             
-            button_layout.addWidget(btn)
+            content_layout.addWidget(btn)
             self.nav_buttons.append(btn)
-            layout.addWidget(button_container)
             
         if self.nav_buttons:
             self.nav_buttons[0].setChecked(True)
             
-        layout.addStretch()
+        content_layout.addStretch()
+        layout.addWidget(content_container)
         
+        # Background styling
         self.setStyleSheet("""
             QWidget {
                 background-color: #EBF1E6;
-
             }
-            QWidget#sidebar {
-                min-height: 100vh;
+            QWidget#content {
+                background-color: #EBF1E6;
             }
         """)
-        self.setObjectName("sidebar")
+        content_container.setObjectName("content")
         
     def handle_button_click(self, index):
         for i, btn in enumerate(self.nav_buttons):
             btn.setChecked(i == index)
         self.pageChanged.emit(index)
-
-
-
-                        # border-top-right-radius: 12px;    
-                        # border-bottom-right-radius: 12px;
-                        # border-top-left-radius: 12px;
-                        # border-bottom-left-radius: 12px;
-
-
-
-                #                         border-top-right-radius: 20px;
-                # border-bottom-right-radius: 20px;
+                   
