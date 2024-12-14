979f118893c2bfae4c15fa0ee133b1fe22ba9bb5
WwzFwz <130327517+WwzFwz@users.noreply.github.com>
2024-12-14 23:20:47 +0700

    chore : code structuring

--- src/frontend/window.py
+++ src/frontend/window.py
@@ -1,7 +1,6 @@
-# src/frontend/window.py
 from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QStackedWidget
 from .components.sidebar import Sidebar  
-from .pages.daftar_tanaman import TanamanUI  # Ganti KelolaTanaman menjadi TanamanUI sesuai perubahan nama class sebelumnya
+from .pages.daftar_tanaman import TanamanUI  
 from .pages.catatan_perkembangan import CatatanPerkembangan
 from .pages.jadwal_perawatan import JadwalUI
 
@@ -10,14 +9,11 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Grootopia")
         self.setGeometry(100, 100, 1920, 1080)
-
         main_widget = QWidget()
         self.setCentralWidget(main_widget)
         layout = QHBoxLayout(main_widget)
-
         self.sidebar = Sidebar()
         self.sidebar.pageChanged.connect(self.change_page)
-
         self.stack = QStackedWidget()
         self.stack.addWidget(TanamanUI())  
         self.stack.addWidget(CatatanPerkembangan())
